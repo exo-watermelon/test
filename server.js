efd00e601e65f29b98f8a3a7288b38af0fc2417a
exo-watermelon <playgameeneo@gmail.com>
2026-06-28 22:17:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,9 +18,16 @@ const io = new Server(server, {
 });
 const players = {};
 io.on('connection', (socket) => {
+    players[socket.id] = {
+        id: socket.id,
+        x: 0,
+        y: 0,
+        z: 0,
+        ry: 0,
+        colors: { head: '#e0e0e0', torso: '#e0e0e0', leftArm: '#e0e0e0', rightArm: '#e0e0e0', leftLeg: '#e0e0e0', rightLeg: '#e0e0e0' }
+    };
     socket.emit('currentPlayers', players);
-    socket.broadcast.emit('newPlayer', { id: socket.id, x: 0, y: 0, z: 0, ry: 0 });
-    players[socket.id] = { id: socket.id, x: 0, y: 0, z: 0, ry: 0 };
+    socket.broadcast.emit('newPlayer', players[socket.id]);
     socket.on('playerMovement', (movementData) => {
         if (players[socket.id]) {
             players[socket.id].x = movementData.x;
@@ -28,6 +35,12 @@ io.on('connection', (socket) => {
             players[socket.id].z = movementData.z;
             players[socket.id].ry = movementData.ry;
             socket.broadcast.emit('playerMoved', players[socket.id]);
+        }
+    });
+    socket.on('playerColorUpdate', (colors) => {
+        if (players[socket.id]) {
+            players[socket.id].colors = colors;
+            socket.broadcast.emit('playerColorsChanged', { id: socket.id, colors });
         }
     });
     socket.on('disconnect', () => {
